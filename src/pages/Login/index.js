b50955329a069d61e5760a2757d426e4f60f6d97
arthurazevedo <arthur.stevam.costa@ccc.ufcg.edu.br>
2020-05-13 16:53:36 -0300

    Login configurado

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
 import {
+  GoogleSignin,
   GoogleSigninButton,
 } from '@react-native-community/google-signin';
+
+import api from '../../config/api';
+
 import styles from './styles';
 
 export default function Login() {
-  function handleLogin() {
-    console.log('foi sim');
-  }
+  const [key, setKey] = useState('');
+
+  GoogleSignin.configure({
+    webClientId: '930287943905-pfin76osh7e8jt4oomji5bcnpvro0sfk.apps.googleusercontent.com',
+  });
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      await GoogleSignin.signIn();
+
+      const { accessToken } = await GoogleSignin.getTokens();
+
+      const user = await api({
+        method: 'POST',
+        url: '/login',
+        headers: {
+          authorization: accessToken,
+          access_key: key,
+        },
+      });
+
+      setKey('');
+
+      console.log(user.data, key);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -19,11 +49,16 @@ export default function Login() {
         <Text style={styles.bemVindo}>
           Bem-vindo realize seu login usando sua conta Google.
         </Text>
-        <TextInput style={styles.input} placeholder="Key..." />
+        <TextInput
+          defaultValue={key}
+          onChangeText={(texto) => setKey(texto)}
+          style={styles.input}
+          placeholder="Key..."
+        />
         <Text style={styles.info}>
           Caso seja seu primeiro login informe a chave de acesso.
         </Text>
-        <GoogleSigninButton onPress={() => handleLogin()} style={styles.googleButton} />
+        <GoogleSigninButton onPress={() => signIn()} style={styles.googleButton} />
       </View>
     </View>
   );

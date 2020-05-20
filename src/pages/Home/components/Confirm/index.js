3de03f8d01c06e229160eafbd63d4762241c4c67
arthurazevedo <arthur.stevam.costa@ccc.ufcg.edu.br>
2020-05-20 19:35:25 -0300

    Confirmação de deletar produto

---
+++ src/pages/Home/components/Confirm/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Dialog, Paragraph, Button } from 'react-native-paper';
+
+export default function Confirm({
+  visible, onPressConfirm, onPressCancel, produto,
+}) {
+  return (
+    <Dialog visible={visible} onDismiss={onPressCancel}>
+      <Dialog.Title>Deletar</Dialog.Title>
+      <Dialog.Content>
+        <Paragraph>
+          {' '}
+          Deseja realmente deletar o produto
+          {produto.name}
+        </Paragraph>
+      </Dialog.Content>
+      <Dialog.Actions>
+        <Button onPress={() => onPressConfirm()}> Deletar</Button>
+        <Button onPress={() => onPressCancel()}>Cancelar</Button>
+      </Dialog.Actions>
+    </Dialog>
+  );
+}
